701f718dbba5a6a4378702f9d85b6bced6e2a6d4
kmkm0318 <kmkm0318@naver.com>
2024-06-08 22:40:48 +0900

    ShowFriendOnMap 변경

--- app/src/main/java/com/example/project/Function/ShowFriendOnMap.kt
+++ app/src/main/java/com/example/project/Function/ShowFriendOnMap.kt
@@ -2,6 +2,6 @@ package com.example.project.Function
 
 import android.util.Log
 
-fun ShowFriendOnMap() {
-    Log.i("ShowFriendonMap", "ShowFriendonMap")
+fun ShowFriendOnMap(lat:Double, lng:Double) {
+    Log.i("showFriendonMap", "lat : $lat , lng : $lng")
 }
